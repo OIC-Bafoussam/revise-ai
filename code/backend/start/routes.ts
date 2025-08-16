30cad463e152b47a8a8b7a12b2eaac9be7f31cd3
Mesha vegas <meshavegas@gmail.com>
2025-08-16 21:11:05 +0100

    Implement authentication controller with registration, login, and user retrieval endpoints

--- code/backend/start/routes.ts
+++ code/backend/start/routes.ts
@@ -8,9 +8,21 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AuthController from '#controllers/auth_controller'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+// Groupe d'authentification
+router.group(() => {
+  router.post('/register', [AuthController, 'register'])
+  router.post('/login', [AuthController, 'login'])
+  // router.post('/logout', [AuthController, 'logout']).use(['auth'])
+  router.get('/me', [AuthController, "me"]).use(middleware.auth({
+    guards:["api"]
+  }))
+}).prefix('api/auth')
