2a27a51d098f065b123ca51de819d947bee2f319
Mesha vegas <meshavegas@gmail.com>
2025-08-16 21:17:49 +0100

    Refactor routes to organize protected routes and ensure consistent middleware usage

--- code/backend/start/routes.ts
+++ code/backend/start/routes.ts
@@ -23,6 +23,30 @@ router.group(() => {
   router.post('/login', [AuthController, 'login'])
   // router.post('/logout', [AuthController, 'logout']).use(['auth'])
   router.get('/me', [AuthController, "me"]).use(middleware.auth({
-    guards:["api"]
+    guards: ["api"]
   }))
 }).prefix('api/auth')
+
+// Groupe de routes protégées
+router.group(() => {
+  // send file
+  router.group(() => {
+    router.post('/upload', () => { })
+  }).prefix('api/file')
+  // generate quiz
+  router.group(() => {
+    router.post('/generate-quiz', () => { })
+  }).prefix('api/quiz')
+  // generate flashcards
+  router.group(() => {
+    router.post('/generate-flashcards', () => { })
+  }).prefix('api/flashcards')
+  // generate summary
+  router.group(() => {
+    router.post('/generate-summary', () => { })
+  }).prefix('api/summary')
+})
+  .use(middleware.auth({
+    guards: ['api'],
+  }))
+  .prefix('api')
